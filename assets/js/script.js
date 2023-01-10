7f9b564dcfb8fc872645e92058a62003c90b411c
SohailMohammed1 <sohailm2233@gmail.com>
2023-01-10 18:44:09 +0000

    Made dynamic answers and updated javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,84 @@
 //Get button element and add event listeners
 //Creating return Element button, also returns elements as Array which can be iterated
+const questions = [
+    {
+        'question': 'What is the Mitchondria?',
+        'option1': 'Powerhouse of the celll',
+        'option2': 'Option 2',
+        'option3': 'Option 3',
+        'option4': 'Option 4',
+        'answer': 'option1'
+    },
+    {
+        'question': 'Question 2',
+        'option1': 'Option 1',
+        'option2': 'Option 2',
+        'option3': 'Option 3',
+        'option4': 'Option 4',
+        'answer': 'option3'
+    },
+    {
+        'question': 'Question 3',
+        'option1': 'Option 1',
+        'option2': 'Option 2',
+        'option3': 'Option 3',
+        'option4': 'Option 4',
+        'answer': 'option2'
+    },
+    {
+        'question': 'Question 4',
+        'option1': 'Option 1',
+        'option2': 'Option 2',
+        'option3': 'Option 3',
+        'option4': 'Option 4',
+        'answer': 'option2'
+    },     
+    
+];
+
+let questionIndex = 0;
+
+function displayScienceQuestion(index) {
+    const question = questions[index];
+    document.getElementById('question').textContent = question.question;
+    document.getElementById('option-1').textContent = question.option1;
+    document.getElementById('option-2').textContent = question.option2;
+    document.getElementById('option-3').textContent = question.option3;
+    document.getElementById('option-4').textContent = question.option4;
+}
+
+function checkAnswer(buttonIndex) {
+    const answer = questions[questionIndex].answer;
+    console.info('Button clicked');
+    console.info(buttonIndex);
+    console.info('Answer');
+    console.info(answer);
+    if (answer === buttonIndex){
+        console.info('Good');
+    }else{
+        console.info('Bad');
+    }
+    questionIndex = questionIndex + 1;
+    runGame('science');
+
+}
+
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+
+function runGame(gameType) {
+    const questionLength = questions.length;
+    if (questionIndex < questionLength){
+        displayScienceQuestion(questionIndex);
+    }
+    //Create 2 random numbers between 1 and 25
+    // let num1 = Math.floor(Math.random() * 25) + 1;
+    // let num2 = Math.floor(Math.random() * 25) + 1;
+
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
@@ -9,8 +88,8 @@ document.addEventListener("DOMContentLoaded", function() {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
-                let gameType = this.getAttribute("data-type");
-                runGame(gameType)
+                const buttonIndex = this.getAttribute("data-index");
+                checkAnswer(buttonIndex)
 
             }
         }) 
@@ -22,23 +101,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
 })
 
-/**
- * The main game "loop", called when the script is first loaded
- * and after the user's answer has been processed
- */
-
-function rungame(gameType) {
 
 
-    //Create 2 random numbers between 1 and 25
-    // let num1 = Math.floor(Math.random() * 25) + 1;
-    // let num2 = Math.floor(Math.random() * 25) + 1;
 
-}
-
-function checkAnswer() {
-
-}
 
 function calculateCorrectAnswer () {
 
@@ -52,9 +117,7 @@ function incrementWrongAnswers() {
 
 }
 
-function displayScienceQuestion() {
-    
-}
+
 
 function displayTechnologyQuestion() {
  
